f6d50533bd697e9cba553df719d3645a4ab59d0f
Alberto Araújo <albertoafaj@hotmail.com>
2023-04-15 08:48:24 -0300

    feat: exemplify how to test styles

--- src/app/testing-component/testing-component.component.spec.ts
+++ src/app/testing-component/testing-component.component.spec.ts
@@ -26,7 +26,28 @@ fdescribe('TestingComponentComponent', () => {
     expect(title.textContent).toBe('Meu título');
   });
   it('should access element in DOM with nativeElement.querySelector()', () => {
-    let p = fixture.debugElement.nativeElement.querySelector('p');
+    const p = fixture.debugElement.nativeElement.querySelector('p');
     expect(p.textContent).toBe('Meu parágrafo');
   });
+  it('should have a button with yes value and background-color green', () => {
+    const btnYes = fixture.debugElement.query(By.css('.btn-yes')).nativeElement;
+    const style = window.getComputedStyle(btnYes);
+    expect(btnYes.textContent).toBe('Yes');
+    expect(rgbToColorName(style.backgroundColor)).toBe('green');
+  });
+  it('should have a button with no value and background-color red', () => {
+    const btnNo = fixture.debugElement.query(By.css('.btn-no')).nativeElement;
+    const style = window.getComputedStyle(btnNo);
+    expect(btnNo.textContent).toBe('No');
+    expect(rgbToColorName(style.backgroundColor)).toBe('red');
+  });
+
+  function rgbToColorName(rgb: string): string {
+    switch (rgb) {
+      case 'rgb(0, 128, 0)': return 'green';
+      case 'rgb(0, 0, 255)': return 'blue';
+      case 'rgb(255, 0, 0)': return 'red';
+      default: return rgb;
+    }
+  }
 });
